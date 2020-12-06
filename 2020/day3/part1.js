40a4fcd2fa36fe972642d27dbdf5e52f368c92b4
joana.correia <redbugmilk@gmail.com>
2020-12-06 17:06:09 +0000

    part 1 completed

--- 2020/day3/part1.js
+++ 2020/day3/part1.js
@@ -1,37 +1,36 @@
 const { readFile } = require("../utils/file");
 const { convertStringToArray } = require("../utils/arrays");
 
-const convertLine = (line, xPosition) => {
-  if (xPosition < line.length) {
-    return line;
-  }
-  convertLine(`${line}${pattern}`, pattern, xPosition);
+const calculatePosition = (xPosition, lineLength) => {
+  const factor = Math.trunc(xPosition / lineLength);
+  return xPosition - lineLength * factor;
 };
+
 const checkIfIsTree = (line, xPosition) => {
-  const nextPosition = xPosition + 1;
-  const lineMap = convertLine(line, line, nextPosition);
-  console.log(lineMap);
-  return lineMap[nextPosition] === "#";
+  const nextPosition = xPosition;
+  const position = calculatePosition(nextPosition, line.length);
+  return line[position] === "#";
 };
+
 const findTheTrees = (lines) => {
   let trees = 0;
   let initialX = 0;
-  return lines.forEach((element, index) => {
+  lines.forEach((element) => {
     if (initialX !== 0) {
-      console.log(element.length);
       if (checkIfIsTree(element, initialX)) {
         trees++;
       }
     }
     initialX = initialX + 3;
   });
+  return trees;
 };
 
 const numberOfTrees = (map) => {
   const lines = convertStringToArray(map);
   const trees = findTheTrees(lines);
 
-  return trees && wrongPasswords.filter(Boolean).length;
+  return trees;
 };
 
 const countingTrees = async () => {
